d212c2ae4ffed740a57811907f7b19b780cf4ff2
Syed Abdul Haliq <haliq.syedabdulmalik@maybank.com>
2023-12-22 13:38:01 +0800

    fix import app.tsx

--- apps/mobile-app/src/app/App.tsx
+++ apps/mobile-app/src/app/App.tsx
@@ -1,11 +1,10 @@
 import '@bintang-bank/shared/styles/unistyles';
 import React from 'react';
 
-import { SomethingWrongPage } from '@bintang-bank/pages';
+import { SomethingWrongPage, AppNavigator } from '@bintang-bank/pages';
 import { persistor, store } from '@bintang-bank/shared';
 import { Provider as StoreProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { AppNavigator } from '@bintang-bank/pages';
 
 export const App = () => {
   return (
