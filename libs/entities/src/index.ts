25c43394fb11901b94b47518d7578a43832c08cb
Syed Abdul Haliq <haliq.syedabdulmalik@maybank.com>
2023-12-23 01:01:38 +0800

    add user, auth slices

--- libs/entities/src/index.ts
+++ libs/entities/src/index.ts
@@ -1,3 +1,5 @@
-export * from './counters/counterSlice';
-export * from './auth/lib/useAuth';
+export * from './auth/interfaces/AuthState';
 export * from './auth/lib/getErrorText';
+export * from './auth/lib/useAuth';
+export * from './auth/slices/authSlice';
+export * from './users/slices/userSlice';
