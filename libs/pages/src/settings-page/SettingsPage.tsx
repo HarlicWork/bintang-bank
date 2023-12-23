dd4803e55198693c64db90b93cdfc01214dccec5
Syed Abdul Haliq <haliq.syedabdulmalik@maybank.com>
2023-12-24 07:53:18 +0800

    fix typo

--- libs/pages/src/settings-page/SettingsPage.tsx
+++ libs/pages/src/settings-page/SettingsPage.tsx
@@ -15,7 +15,7 @@ export function SettingsPage(props: SettingsPageProps) {
         <Typo
           screen={['settings']}
           text="greetings"
-          color={theme.colors.onTypography}
+          color={theme.colors.primary}
           preset="h3"
         />
       </View>
