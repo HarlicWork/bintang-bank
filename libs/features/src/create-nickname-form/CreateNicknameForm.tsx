61f394f821887e987e245baabfbc25736efb6e9d
Syed Abdul Haliq <haliq.syedabdulmalik@maybank.com>
2023-12-24 23:03:52 +0800

    add react-hook-form

--- libs/features/src/create-nickname-form/CreateNicknameForm.tsx
+++ libs/features/src/create-nickname-form/CreateNicknameForm.tsx
@@ -1,44 +1,86 @@
 import { useChangeNickname } from '@bintang-bank/entities';
-import { Button, TextInput, logger } from '@bintang-bank/shared';
-import { useState } from 'react';
+import { Button, TextInput, Typo, logger } from '@bintang-bank/shared';
+import { Controller, FieldError, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
-import { KeyboardAvoidingView, Platform } from 'react-native';
+import { KeyboardAvoidingView, Platform, View } from 'react-native';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
 /* eslint-disable-next-line */
 export interface CreateNicknameFormProps {}
 
 export function CreateNicknameForm(props: CreateNicknameFormProps) {
-  const { styles } = useStyles(stylesheet);
+  const { styles, theme } = useStyles(stylesheet);
   const { t } = useTranslation(['createDisplayName', 'common']);
   const { changeNickname } = useChangeNickname();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      nickname: '',
+    },
+  });
 
-  const [nickname, setNickname] = useState<string>('');
-
-  const onPressChangeNickname = () => {
-    logger('press');
-    changeNickname(nickname);
+  const onErrorMessage = (nickname: FieldError | undefined) => {
+    switch (nickname?.type) {
+      case 'pattern':
+        return 'nickname_error_pattern';
+      case 'minLength':
+        return 'nickname_error_minLength';
+      default:
+        return 'nickname_error_required';
+    }
   };
+
+  const onPressChangeNickname = handleSubmit((data) => {
+    logger('data', data);
+    changeNickname(data.nickname);
+  });
 
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={styles.container}
     >
-      <TextInput
-        style={styles.nicknameTextInput}
-        onChangeText={(text) => setNickname(text)}
-        placeholder={t('createDisplayName:nickname')}
-        value={nickname}
-        autoCapitalize="none"
-        editable={true}
-      />
+      <View style={styles.nicknameContainer}>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+            minLength: 3,
+            maxLength: 20,
+            pattern: /^[A-Za-z]+$/,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={styles.nicknameTextInput}
+              onChangeText={onChange}
+              placeholder={t('createDisplayName:nickname')}
+              value={value}
+              autoCapitalize="none"
+              editable={true}
+            />
+          )}
+          name="nickname"
+        />
+        {errors.nickname && (
+          <Typo
+            text={onErrorMessage(errors.nickname)}
+            screen={['createDisplayName']}
+            color={theme.colors.error}
+            preset={'h4'}
+          />
+        )}
+      </View>
 
-      <Button
-        title={t('common:common.proceed')}
-        onPress={onPressChangeNickname}
-      />
+      <View style={styles.btnContainer}>
+        <Button
+          title={t('common:common.proceed')}
+          onPress={onPressChangeNickname}
+        />
+      </View>
     </KeyboardAvoidingView>
   );
 }
@@ -48,7 +90,12 @@ const stylesheet = createStyleSheet((theme) => ({
     flex: 1,
     marginTop: 32,
     flexDirection: 'column',
+    alignItems: 'center',
     justifyContent: 'space-between',
+  },
+  nicknameContainer: {
+    gap: 10,
+    alignItems: 'center',
   },
   nicknameTextInput: {
     height: 40,
@@ -58,6 +105,9 @@ const stylesheet = createStyleSheet((theme) => ({
     borderRadius: 10,
     borderWidth: 1,
     color: theme.colors.onBackground,
+  },
+  btnContainer: {
+    width: 200,
   },
 }));
 
