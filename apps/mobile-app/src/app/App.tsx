edebb2d3be042a9523c6af91bd6856692fdf0bcf
Syed Abdul Haliq <haliq.syedabdulmalik@maybank.com>
2023-12-29 13:46:33 +0800

    add suspend

--- apps/mobile-app/src/app/App.tsx
+++ apps/mobile-app/src/app/App.tsx
@@ -1,12 +1,17 @@
 import '@bintang-bank/shared/styles/unistyles';
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 
 import { persistor, store } from '@bintang-bank/entities/store/store';
-import { AppNavigator, SomethingWrongPage } from '@bintang-bank/pages';
+import { SomethingWrongPage } from '@bintang-bank/pages';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Provider as StoreProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ActivityIndicator } from 'react-native';
+
+const AppNavigator = lazy(
+  () => import('../../../../libs/pages/src/navigators/AppNavigator')
+);
 
 export const App = () => {
   return (
@@ -14,7 +19,11 @@ export const App = () => {
       <PersistGate loading={<SomethingWrongPage />} persistor={persistor}>
         <GestureHandlerRootView style={{ flex: 1 }}>
           <BottomSheetModalProvider>
-            <AppNavigator />
+            <Suspense
+              fallback={<ActivityIndicator size="large" color="#0000ff" />}
+            >
+              <AppNavigator />
+            </Suspense>
           </BottomSheetModalProvider>
         </GestureHandlerRootView>
       </PersistGate>
