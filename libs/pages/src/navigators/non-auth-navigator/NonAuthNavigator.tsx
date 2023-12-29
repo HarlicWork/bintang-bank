8dac7894247723f26d9b4110834e142895d476ef
Syed Abdul Haliq <haliq.syedabdulmalik@maybank.com>
2023-12-29 19:13:13 +0800

    refactor routes using map

--- libs/pages/src/navigators/non-auth-navigator/NonAuthNavigator.tsx
+++ libs/pages/src/navigators/non-auth-navigator/NonAuthNavigator.tsx
@@ -1,25 +1,41 @@
-import { AppRoutes, RootStackParamList } from '@bintang-bank/shared';
+import { AppRoutes } from '@bintang-bank/shared';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import LoginPage from '../../login-page/LoginPage';
 import SomethingWrongPage from '../../something-wrong-page/SomethingWrongPage';
 import StartupPage from '../../startup-page/StartupPage';
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createNativeStackNavigator();
 
 /* eslint-disable-next-line */
 export interface NonAuthNavigatorProps {}
 
 export function NonAuthNavigator(props: NonAuthNavigatorProps) {
+  const screens = [
+    {
+      name: AppRoutes.Startup,
+      component: StartupPage,
+    },
+    {
+      name: AppRoutes.Login,
+      component: LoginPage,
+    },
+    {
+      name: AppRoutes.SomethingWrong,
+      component: SomethingWrongPage,
+    },
+  ] as const;
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Group>
-        <Stack.Screen name={AppRoutes.Startup} component={StartupPage} />
-        <Stack.Screen name={AppRoutes.Login} component={LoginPage} />
-        <Stack.Screen
-          name={AppRoutes.SomethingWrong}
-          component={SomethingWrongPage}
-        />
+        {screens.map((screen, index) => (
+          <Stack.Screen
+            key={index}
+            name={screen.name}
+            component={screen.component}
+          />
+        ))}
       </Stack.Group>
     </Stack.Navigator>
   );
