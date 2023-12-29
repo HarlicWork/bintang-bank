f6028a7fb5aaee6d1ab117a6f929ba8904798f5f
Syed Abdul Haliq <Harlic.work@gmail.com>
2023-12-29 11:49:01 +0800

    update img (#28)
    Co-authored-by: Syed Abdul Haliq <haliq.syedabdulmalik@maybank.com>

--- libs/assets/src/index.ts
+++ libs/assets/src/index.ts
@@ -2,6 +2,7 @@ const assets = {
   // images
   bankLogo: require('./images/bankLogo.png'),
   error404: require('./images/error404.png'),
+  offline: require('./images/going_offline.png'),
 };
 
 export default assets;
